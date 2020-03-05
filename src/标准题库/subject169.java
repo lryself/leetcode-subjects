a389c2d4dc2adbfb52228b15bde34d29c819532a
lryself <lnolvwe@163.com>
2020-03-05 18:33:16 +0800

    Initial commit

---
+++ src/标准题库/subject169.java
@@ -0,0 +1,18 @@
+package 标准题库;
+
+import java.util.Arrays;
+
+public class subject169 {
+    public static void main(String args[])
+    {
+        int nums[]=new int[]{6,6,6,7,7};
+        Solution solution=new Solution();
+        System.out.println(solution.majorityElement(nums));
+    }
+}
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
