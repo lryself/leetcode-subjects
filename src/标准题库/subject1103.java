8eb217071d3a81735cae0c54ff98eae9937bc04c
lryself <lnolvwe@163.com>
2020-03-06 20:53:01 +0800

    20200306做题

--- src/标准题库/subject1103.java
+++ src/标准题库/subject1103.java
@@ -6,29 +6,34 @@ public class subject1103 {
     public static void main(String args[])
     {
         Solution solution=new Solution();
-        int candies = 7, num_people = 4;
+        int candies = 90, num_people = 4;
         int ans[]=solution.distributeCandies(candies,num_people);
         System.out.println(Arrays.toString(ans));
     }
     static class Solution {
         public int[] distributeCandies(int candies, int num_people) {
             int ans[]=new int[num_people];
-            int now=0,i=1;
-            while (candies>0)
+            int turns=(int)(Math.sqrt(candies*2+1/4)-1/2);
+            int c=candies-(1+turns)*turns/2;
+            if(c<0)
             {
-                now%=num_people;
-                if(candies<i)
-                {
-                    ans[now]+=candies;
-                    break;
-                }
-                else {
-                    candies-=i;
-                    ans[now]+=i;
-                }
-                i++;
-                now++;
+                turns--;
+                c=candies-(1+turns)*turns/2;
             }
+            int a=turns/num_people;
+            int b=turns%num_people;
+            if(a>0)
+            {
+                for(int i=0;i<num_people;i++)
+                {
+                    ans[i]+=((i+1)*2+(a-1)*num_people)*a/2;
+                }
+            }
+            for(int i=0;i<b;i++)
+            {
+                ans[i]+=i+1+a*num_people;
+            }
+            ans[b]+=c;
             return ans;
         }
     }
---
+++ src/面试题/subject57.java
@@ -0,0 +1,84 @@
+package 面试题;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class subject57 {
+    public static void main(String args[])
+    {
+        int sum=93;
+        Solution solution=new Solution();
+        int a[][]=solution.findContinuousSequence(sum);
+        for(int[] b:a)
+        {
+            for(int c:b)
+            {
+                System.out.print(c+" ");
+            }
+            System.out.println();
+        }
+    }
+    static class Solution {
+        public int[][] findContinuousSequence(int target) {
+            ArrayList<int[]>intlist=new ArrayList<>();
+            if(target%2!=0)
+                intlist.add(new int[]{target/2,target/2+1});
+            for(int i=3;target/i-i/2>=0;i++)
+            {
+                int num1[]=new int[i];
+                if(i%2==0)
+                {
+                    num1[0]=target/i;
+                    num1[1]=num1[0]+1;
+                    if(target%(num1[0]+num1[1])!=0)
+                        continue;
+                }
+                else
+                {
+                    if(target/i-i/2<=0)
+                        continue;
+                    if(target%i!=0)
+                        continue;
+                    num1[0]=target/i;
+                }
+                for(int j=1,w=1;j<i;j+=2,w++)
+                {
+                    num1[j]=num1[0]+w;
+                    if((j+1)<num1.length)
+                        num1[j+1]=num1[0]-w;
+                }
+                Arrays.sort(num1);
+                intlist.add(num1);
+            }
+            int b[][]=new int[intlist.size()][];
+            for (int i=0;i<intlist.size();i++) {
+                b[i]=intlist.get(i);
+            }
+            qsort(b,0,b.length-1);
+            return b;
+        }
+        void qsort(int[][] a, int left, int right) {
+            if (left>right) return ;
+            int left_pointer = left;
+            int right_pointer = right;
+            int key = a[left][0];
+            while (left_pointer < right_pointer) {
+                while (left_pointer < right_pointer && key < a[right_pointer][0]) {//从右边开始找小于key的值
+                    right_pointer--;
+                }
+                int temp[]=a[left_pointer];
+                a[left_pointer] = a[right_pointer];//如果找到交换，否则就是left_pointer = right_pointer
+                a[right_pointer]=temp;
+                while (left_pointer < right_pointer && key > a[left_pointer][0]) {
+                    left_pointer++;
+                }
+                temp=a[right_pointer];
+                a[right_pointer] = a[left_pointer];//如果找到交换，否则就是left_pointer = right_pointer
+                a[left_pointer]=temp;
+            }
+            a[left_pointer][0] = key;//一趟排序完毕，key放入指针位置，这时left_pointer = righ_pointer，key左边都比它小，右边都比它大，然后各自递归左右两边
+            qsort(a, left, left_pointer - 1);
+            qsort(a, left_pointer + 1, right);
+        }
+    }
+}
