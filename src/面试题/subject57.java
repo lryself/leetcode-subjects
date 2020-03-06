3ac3f0ce06798785f8251ea737e7870bf3a35fe9
lryself <lnolvwe@163.com>
2020-03-06 21:25:57 +0800

    20200306做题

--- src/面试题/subject57.java
+++ src/面试题/subject57.java
@@ -6,9 +6,21 @@ import java.util.Arrays;
 public class subject57 {
     public static void main(String args[])
     {
-        int sum=93;
-        Solution solution=new Solution();
-        int a[][]=solution.findContinuousSequence(sum);
+        int all=15;
+        Solution1 solution1=new Solution1();
+        Solution2 solution2=new Solution2();
+        int a[][]=solution1.findContinuousSequence(all);
+        System.out.println("方法一：");
+        for(int[] b:a)
+        {
+            for(int c:b)
+            {
+                System.out.print(c+" ");
+            }
+            System.out.println();
+        }
+        a=solution2.findContinuousSequence(all);
+        System.out.println("方法二：");
         for(int[] b:a)
         {
             for(int c:b)
@@ -18,7 +30,8 @@ public class subject57 {
             System.out.println();
         }
     }
-    static class Solution {
+    //方法一：
+    static class Solution1 {
         public int[][] findContinuousSequence(int target) {
             ArrayList<int[]>intlist=new ArrayList<>();
             if(target%2!=0)
@@ -79,6 +92,34 @@ public class subject57 {
             a[left_pointer][0] = key;//一趟排序完毕，key放入指针位置，这时left_pointer = righ_pointer，key左边都比它小，右边都比它大，然后各自递归左右两边
             qsort(a, left, left_pointer - 1);
             qsort(a, left_pointer + 1, right);
+        }
+    }
+//方法二：
+    static class Solution2 {
+        public int[][] findContinuousSequence(int target) {
+            int r=(int)Math.sqrt(2*target);
+            int[][] nums=new int[r][];
+            int s=0;
+            for(int n=2;r>=n;n++)
+            {
+                int temp1=target-n*(n-1)/2;
+                if(temp1%n==0)
+                {
+                    int num[]=new int[n];
+                    int temp=temp1/n;
+                    for(int i=0;i<n;i++)
+                    {
+                        num[i]=i+temp;
+                    }
+                    nums[s++]=num;
+                }
+            }
+            int result[][]=new int[s][];
+            for(int i=0;i<s;i++)
+            {
+                result[i]=nums[s-1-i];
+            }
+            return result;
         }
     }
 }
