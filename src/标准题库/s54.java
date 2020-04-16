599e086fed201dbaefd1f9092b47c0d4b833b31e
lryself <lnolvwe@163.com>
2020-04-16 20:03:06 +0800

    54题解决

--- src/标准题库/s54.java
+++ src/标准题库/s54.java
@@ -5,31 +5,39 @@ import java.util.*;
 public class s54 {
     static class Solution {
         public int[][] merge(int[][] intervals) {
-            Queue<int[]> dq=new PriorityQueue<>(cmp);
-            for(int[] i:intervals)  dq.offer(i);
+            if(intervals.length==0)
+                return intervals;
+            Arrays.sort(intervals, new MyComprator());
             int[][] ans =new int[intervals.length][];
             int w=0;
-            while(!dq.isEmpty())
+            ans[0]=intervals[0];
+            for(int i=1;i<ans.length;i++)
             {
-                int[] r1=dq.poll();
-                if(dq.isEmpty())
-                    ans[w++]=r1;
-                else if(r1[1]>=dq.peek()[0])
-                {
-                    dq.offer(new int[]{r1[0],Math.max(r1[1],dq.poll()[1])});
-                }
+                if(ans[w][1]>=intervals[i][0])
+                    ans[w][1]=Math.max(ans[w][1],intervals[i][1]);
                 else
-                {
-                    ans[w++]=r1;
-                }
+                    ans[++w]=intervals[i];
             }
-            return Arrays.copyOfRange(ans,0,w);
+            return Arrays.copyOfRange(ans,0,w+1);
         }
-        Comparator<int[]> cmp = new Comparator<int[]>() {
-            public int compare(int[] e1, int[] e2) {
-                return e1[0] - e2[0];
+        static class MyComprator implements Comparator {
+            public int compare(Object arg0, Object arg1) {
+                if(arg0==null&&arg1==null)
+                    return 0;
+                if(arg0==null)
+                    return -1;
+                if(arg1==null)
+                    return 1;
+                int[] t1=(int[])arg0;
+                int[] t2=(int[])arg1;
+                if(t1[0]>t2[0])
+                    return 1;
+                else if(t1[0]<t2[0])
+                    return -1;
+                else
+                    return 0;
             }
-        };
+        }
     }
 
     public static void main(String[] args) {
