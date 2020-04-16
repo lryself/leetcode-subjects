d5c504f5f72ce791f75ddf250ab331ede068e8d8
lryself <lnolvwe@163.com>
2020-04-16 19:41:41 +0800

    54题解决

--- src/标准题库/s54.java
+++ src/标准题库/s54.java
@@ -1,38 +1,35 @@
 package 标准题库;
 
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class s54 {
     static class Solution {
         public int[][] merge(int[][] intervals) {
             Queue<int[]> dq=new PriorityQueue<>(cmp);
             for(int[] i:intervals)  dq.offer(i);
-            Queue<int[]> res=new LinkedList<>();
+            int[][] ans =new int[intervals.length][];
+            int w=0;
             while(!dq.isEmpty())
             {
                 int[] r1=dq.poll();
                 if(dq.isEmpty())
-                    res.offer(r1);
+                    ans[w++]=r1;
                 else if(r1[1]>=dq.peek()[0])
                 {
                     dq.offer(new int[]{r1[0],Math.max(r1[1],dq.poll()[1])});
                 }
                 else
                 {
-                    res.offer(r1);
+                    ans[w++]=r1;
                 }
             }
-            int n=res.size();
-            int[][] ans =new int[n][];
-            for(int i=0;i<n;i++)
-            {
-                ans[i]=res.poll();
-            }
-            return ans;
+            return Arrays.copyOfRange(ans,0,w);
         }
+        Comparator<int[]> cmp = new Comparator<int[]>() {
+            public int compare(int[] e1, int[] e2) {
+                return e1[0] - e2[0];
+            }
+        };
     }
 
     public static void main(String[] args) {
@@ -48,9 +45,4 @@ public class s54 {
             System.out.println();
         }
     }
-    static Comparator<int[]> cmp = new Comparator<int[]>() {
-        public int compare(int[] e1, int[] e2) {
-            return e1[0] - e2[0];
-        }
-    };
 }
